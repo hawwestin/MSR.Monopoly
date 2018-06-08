07fa0f137748dc6bda615c0ef2703cf33a0cf07f
hawwestin <michal.robaszewski+git@gmail.com>
2018-06-08 23:41:08 +0200

    add Visible JPanel Toggle #4

--- Monopoly/src/UI/Start.java
+++ Monopoly/src/UI/Start.java
@@ -5,14 +5,26 @@
  */
 package UI;
 
+import Core.GameState;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Michal
  */
 public class Start extends JFrame {
+
+    private static GameWindow game;
+    private static GameState gameState;
+    private static JPanel mainPanel;
+    private static WelcomeWindow welcome;
+
+    public static GameWindow getGame() {
+        return game;
+    }
 
     public static void main(String[] args) {
         new Start();
@@ -21,10 +33,41 @@ public class Start extends JFrame {
     public Start() {
         setTitle("Monopoly");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setContentPane(new GameWindow());
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        game = new GameWindow();
+        welcome = new WelcomeWindow(this);
+
+        changeState(GameState.START_STATE);
+
+        setContentPane(mainPanel);
+
         pack();
         setVisible(true);
-        setSize(new Dimension(800,600));
+        setSize(new Dimension(800, 600));
     }
 
+    public static void changeState(GameState state) {
+        gameState = state;
+
+        switch (state) {
+            case START_STATE:
+                mainPanel.removeAll();
+                mainPanel.add(welcome);
+                mainPanel.revalidate();
+                mainPanel.repaint();
+                break;
+            case GAME_STATE:
+                mainPanel.removeAll();
+                mainPanel.add(game);
+                mainPanel.revalidate();
+                mainPanel.repaint();
+                break;
+               //todo WinnerState with new Sth! ?
+            default:
+                System.out.println("UNKNOWN STATE!");
+                break;
+        }
+    }
 }
